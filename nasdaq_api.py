7ca367b31f5dc12bd24123251a7e28de4f26ce42
Hasith <hasith200311@gmail.com>
2022-06-11 16:03:08 -0500

    intial commit

---
+++ nasdaq_api.py
@@ -0,0 +1,1 @@
+# input ticker and read json form api to output price history and analyze in moving_average.py
---
+++ read_csv.py
@@ -0,0 +1,7 @@
+# read consitutients.csv and store as list to feed into nasdaq_api.py
+import pandas as pd
+
+
+def get_csv_tickers(file):
+    data = pd.read_csv(file).Symbol
+    return data
