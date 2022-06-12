941d6a2f4103a07d0583522c0c7487e91d03d5d7
AV912 <88800484+AV912@users.noreply.github.com>
2022-06-12 16:41:03 -0500

    api created

--- nasdaq_api.py
+++ nasdaq_api.py
@@ -1,1 +1,40 @@
 # input ticker and read json form api to output price history and analyze in moving_average.py
+
+
+import pandas as pd
+from polygon import RESTClient
+from typing import cast
+from urllib3 import HTTPResponse
+import datetime
+
+
+
+
+def getHistoricalData(ticker):
+    client = RESTClient("9P7ogpJgyLF1lauT17eqrTAL7YiKriH0") # api_key is used
+
+    year_ago = datetime.date.today() - datetime.timedelta(days=365)
+    aggs = cast(
+        HTTPResponse,
+        client.get_aggs(
+            ticker,
+            1,
+            "day",
+            year_ago.strftime("%Y-%m-%d"),
+            datetime.date.today().strftime("%Y-%m-%d"),
+            raw=True,
+        ),
+    )
+
+    return aggs.data
+
+
+
+
+if __name__ == '__main__':
+    print(getHistoricalData("AAPL"))
+
+
+
+
+
