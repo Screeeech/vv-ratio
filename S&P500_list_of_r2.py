e7da691c6c89ca133f152e86e14666962f59a145
Hasith <hasith200311@gmail.com>
2022-06-15 16:07:14 -0500

    sending files

---
+++ S&P500_list_of_r2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import csv
+import moving_average as ma
+
+
+def create_r_squared_list():
+    df = pd.read_csv("constituents.csv")
+    f = csv.writer(open("S&P500_r-squared_list.csv", "w", newline=''))
+    f.writerow(["ticker", "r-squared"])
+
+    for symbol in df["Symbol"]:
+        f.writerow([
+            symbol,
+            ma.get_moving_avg_r2(symbol)])
+
+
+if __name__ == '__main__':
+    create_r_squared_list()
